2864d577157b44ab302fb1fcae3bf8b4f368bea9
michaeldimmitt <michaelgdimmitt@gmail.com>
2017-12-30 08:39:25 -0500

    youtube convert from standard to embed

--- make_playlist.sh
+++ make_playlist.sh
@@ -1,4 +1,3 @@
-
 # handle input, planned to be command line but could also be prompting.
 # download sc
 # cut command add embed logic... and autoplay
@@ -9,3 +8,17 @@
 # make into an offbeat brew package?
 #
 # change the name of screensaverprogram via second commnd-line ARGV
+
+#### git clone https://github.com/liquidx/webviewscreensaver.git
+
+# string conversion
+stringy="https://www.youtube.com/watch?v=durCuF6jb8k&list=PLZbIo_dYB77vgAus0es8Ul3kFkRYIRjJc"
+# https://www.youtube.com/embed/durCuF6jb8k?rel=0&autoplay=1&list=PLZbIo_dYB77vgAus0es8Ul3kFkRYIRjJc&index=1&?rel=0&autoplay=1
+
+# cut before "watch?v=" to variable 1
+# cut everything after "watch?v=" to variable 2
+# variable1 + "/embed/" + variable2 + ?rel=0&autoplay=1
+
+var1=$(echo $stringy | cut -c1-23)
+var2=$(echo $stringy | cut -c33-)
+echo $var1"/embed/"$var2"?rel=0&autoplay=1"
