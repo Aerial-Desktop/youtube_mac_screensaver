457168b341e2546bc4eea8a7da34c70adc950f47
michaeldimmitt <michaelgdimmitt@gmail.com>
2017-12-30 11:10:14 -0500

    update plan.

--- make_playlist.sh
+++ make_playlist.sh
@@ -1,12 +1,16 @@
+
 # handle input, planned to be command line but could also be prompting.
+
+#<done>
 # download sc
 # cut command add embed logic... and autoplay
 # sed command insert into that one line.
+#</done>
+
 # test, do they have the environment to compile? if not lead them through the process.
+
 # compile with xcode tools.
-# 
 # make into an offbeat brew package?
-#
 # change the name of screensaverprogram via second commnd-line ARGV
 
  git clone https://github.com/liquidx/webviewscreensaver.git &&
